1d71e0271396273e796c4ae051085e25e3343a65
yulya3102 <yulya3102@gmail.com>
2016-01-18 00:16:12 +0300

    More stubs

--- buffer_tree.cpp
+++ buffer_tree.cpp
@@ -9,3 +9,32 @@ void buffer_tree_t::add(std::int64_t x)
     // TODO
 }
 
+buffer_tree_t::any_node_t buffer_tree_t::load_node(const buffer_tree_t::node_id & id) const
+{
+
+}
+
+buffer_tree_t::leaf_t buffer_tree_t::node_visitor_t::operator () (const leaf_t & leaf) const
+{
+    // for leaf:
+    // remove leaf from filesystem
+    // return leaf
+}
+
+buffer_tree_t::leaf_t buffer_tree_t::node_visitor_t::operator () (const node_t & node) const
+{
+    // for node:
+    // flush
+    // get leaf from left node
+    // if left node does not exist anymore
+    // remove link to it
+    // return received leaf
+}
+
+buffer_tree_t::leaf_t buffer_tree_t::pop_left(const buffer_tree_t::node_id & id)
+{
+    any_node_t node = load_node(id);
+
+    boost::apply_visitor(node_visitor_t(), node);
+}
+
--- buffer_tree.h
+++ buffer_tree.h
@@ -19,17 +19,26 @@ private:
     using any_node_t = boost::variant<node_t, leaf_t>;
     using node_id = boost::filesystem::path;
 
-    any_node_t load_node(const node_id & id);
+    any_node_t load_node(const node_id & id) const;
+    leaf_t pop_left(const node_id & id);
 
     struct node_t
     {
         std::vector<std::int64_t> pending_add;
         std::vector<node_id> children;
+
+        void flush();
     };
 
     struct leaf_t
     {
         std::vector<std::int64_t> elements;
+    };
+
+    struct node_visitor_t : boost::static_visitor<buffer_tree_t::leaf_t>
+    {
+        buffer_tree_t::leaf_t operator () (const leaf_t & leaf) const;
+        buffer_tree_t::leaf_t operator () (const node_t & node) const;
     };
 
     node_id root;
@@ -38,5 +47,11 @@ private:
 template <typename OutIter>
 OutIter buffer_tree_t::fetch_block(OutIter out)
 {
-    // TODO
+    leaf_t leaf = pop_left(root);
+    for (std::int64_t elem : leaf.elements)
+    {
+        *out = elem;
+        ++out;
+    }
+    return out;
 }
