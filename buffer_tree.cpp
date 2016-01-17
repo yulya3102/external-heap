f8e90e524cfb4112323d180afdfb4aa696246d59
yulya3102 <yulya3102@gmail.com>
2016-01-17 09:18:38 +0300

    Add node stubs for buffer tree

--- buffer_tree.cpp
+++ buffer_tree.cpp
@@ -1,9 +1,8 @@
 #include "buffer_tree.h"
 
 buffer_tree_t::buffer_tree_t()
-{
-
-}
+    : root("root")
+{ }
 
 void buffer_tree_t::add(std::int64_t x)
 {
--- buffer_tree.h
+++ buffer_tree.h
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 
+#include <boost/variant.hpp>
+#include <boost/filesystem.hpp>
+
 
 struct buffer_tree_t
 {
@@ -10,6 +13,26 @@ struct buffer_tree_t
 
     template <typename OutIter>
     OutIter fetch_block(OutIter out);
+
+private:
+    struct node_t; struct leaf_t;
+    using any_node_t = boost::variant<node_t, leaf_t>;
+    using node_id = boost::filesystem::path;
+
+    any_node_t load_node(const node_id & id);
+
+    struct node_t
+    {
+        std::vector<std::int64_t> pending_add;
+        std::vector<node_id> children;
+    };
+
+    struct leaf_t
+    {
+        std::vector<std::int64_t> elements;
+    };
+
+    node_id root;
 };
 
 template <typename OutIter>
