9b4b178c266d19e46ee6dfd9a3759396624e5918
yulya3102 <yulya3102@gmail.com>
2016-01-18 01:09:26 +0300

    Take first child from buffer tree

--- buffer_tree.cpp
+++ buffer_tree.cpp
@@ -1,14 +1,16 @@
 #include "buffer_tree.h"
 
-buffer_tree_t::buffer_tree_t()
-{ }
-
 void buffer_tree_t::add(std::int64_t x)
 {
     // TODO
 }
 
-storage_t::any_node_t storage_t::load_node(const storage_t::node_id & id)
+storage_t::any_node_t storage_t::load_node(const storage_t::node_id & id) const
+{
+
+}
+
+void storage_t::delete_node(const storage_t::node_id & id) const
 {
 
 }
@@ -18,27 +20,61 @@ storage_t::node_id storage_t::root_node() const
     return root;
 }
 
-storage_t::leaf_t buffer_tree_t::node_visitor_t::operator () (const storage_t::leaf_t & leaf) const
+namespace
 {
-    // for leaf:
-    // remove leaf from filesystem
-    // return leaf
+struct visitor_result_t
+{
+    storage_t::leaf_t leaf;
+    bool was_removed;
+};
+
+struct node_visitor_t : boost::static_visitor<visitor_result_t>
+{
+    node_visitor_t(const storage_t & storage)
+        : storage(storage)
+    {}
+
+    visitor_result_t operator () (const storage_t::leaf_t & leaf) const;
+    visitor_result_t operator () (storage_t::node_t & node) const;
+
+private:
+    const storage_t & storage;
+};
+
+visitor_result_t node_visitor_t::operator () (const storage_t::leaf_t & leaf) const
+{
+    storage.delete_node(leaf.id);
+    return { leaf, true };
 }
 
-storage_t::leaf_t buffer_tree_t::node_visitor_t::operator () (const storage_t::node_t & node) const
+visitor_result_t node_visitor_t::operator () (storage_t::node_t & node) const
 {
-    // for node:
-    // flush
-    // get leaf from left node
-    // if left node does not exist anymore
-    // remove link to it
-    // return received leaf
+    node.flush();
+
+    storage_t::any_node_t first_child = storage.load_node(node.children.front());
+    visitor_result_t res = boost::apply_visitor(node_visitor_t(storage), first_child);
+    if (res.was_removed)
+        node.children.pop_front();
+
+    // TODO: check if there are no children left
+    bool was_removed = false;
+
+    // TODO: update node on filesystem
+
+    return { res.leaf, was_removed };
+}
 }
 
 storage_t::leaf_t buffer_tree_t::pop_left()
 {
     storage_t::any_node_t node = storage.load_node(storage.root_node());
 
-    return boost::apply_visitor(node_visitor_t(), node);
+    return boost::apply_visitor(node_visitor_t(storage), node).leaf;
 }
 
+
+
+void storage_t::node_t::flush() const
+{
+
+}
--- buffer_tree.h
+++ buffer_tree.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+#include <deque>
+#include <queue>
 
 #include <boost/variant.hpp>
 #include <boost/filesystem.hpp>
@@ -11,17 +13,17 @@ struct storage_t
     using any_node_t = boost::variant<node_t, leaf_t>;
     using node_id = boost::filesystem::path;
 
-    any_node_t load_node(const node_id & id);
-    void delete_node(const node_id & id);
+    any_node_t load_node(const node_id & id) const;
+    void delete_node(const node_id & id) const;
     node_id root_node() const;
 
     struct node_t
     {
         node_id id;
-        std::vector<std::int64_t> pending_add;
-        std::vector<node_id> children;
+        std::queue<std::int64_t> pending_add;
+        std::deque<node_id> children;
 
-        void flush();
+        void flush() const;
     };
 
     struct leaf_t
@@ -36,7 +38,6 @@ private:
 
 struct buffer_tree_t
 {
-    buffer_tree_t();
     void add(std::int64_t x);
 
     template <typename OutIter>
@@ -44,12 +45,6 @@ struct buffer_tree_t
 
 private:
     storage_t::leaf_t pop_left();
-
-    struct node_visitor_t : boost::static_visitor<storage_t::leaf_t>
-    {
-        storage_t::leaf_t operator () (const storage_t::leaf_t & leaf) const;
-        storage_t::leaf_t operator () (const storage_t::node_t & node) const;
-    };
 
     storage_t storage;
 };
