3df818973602284008533177d6d7df34a5ab73ed
yulya3102 <yulya3102@gmail.com>
2016-01-17 08:43:18 +0300

    Add test for random small integers

--- main.cpp
+++ main.cpp
@@ -12,6 +12,22 @@ TEST(small, descending)
         EXPECT_EQ(heap.remove_min(), i);
 }
 
+TEST(small, random)
+{
+    std::default_random_engine generator;
+    std::uniform_int_distribution<std::int64_t> distribution(1, 100);
+    heap_t heap;
+    size_t elements = 512;
+    heap.add(100);
+    for (std::size_t i = 1; i < elements; ++i)
+        heap.add(distribution(generator));
+
+    std::vector<std::int64_t> sorted;
+    for (std::size_t i = 0; i < elements; ++i)
+        sorted.push_back(heap.remove_min());
+    EXPECT_TRUE(std::is_sorted(sorted.begin(), sorted.end()));
+}
+
 int main(int argc, char ** argv)
 {
     testing::InitGoogleTest(&argc, argv);
