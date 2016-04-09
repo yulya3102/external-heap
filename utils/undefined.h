63fd00bb530a6aaeaff84064ee57df3fd7dc13e6
yulya3102 <yulya3102@gmail.com>
2016-04-09 19:43:48 +0300

    Add 'undefined' macro

---
+++ utils/undefined.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define __AT__ __FILE__ ":" TOSTRING(__LINE__)
+
+#define undefined throw std::runtime_error(__AT__ ": not implemented")
