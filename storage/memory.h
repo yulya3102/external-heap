48f06c50bc49eca2906c27b962f74244fbf71d8d
yulya3102 <yulya3102@gmail.com>
2016-04-10 12:20:34 +0300

    Extract common storage interface

--- storage/memory.h
+++ storage/memory.h
@@ -8,8 +8,17 @@ namespace storage
 {
 using node_id = std::uint64_t;
 
+template <typename Serialized>
+struct basic_storage
+{
+    virtual node_id new_node() const = 0;
+    virtual std::shared_ptr<Serialized> load_node(const node_id & id) const = 0;
+    virtual void delete_node(const node_id & id) = 0;
+    virtual void write_node(const node_id & id, Serialized * node) = 0;
+};
+
 template <typename Node>
-struct memory
+struct memory : basic_storage<Node>
 {
     memory(std::function<Node *(Node *)> copy = [] (Node * node) -> Node * { return new Node(*node); })
         : copy_(copy)
@@ -22,23 +31,23 @@ struct memory
             storage_[x.first].reset(copy_(x.second.get()));
     }
 
-    node_id new_node() const
+    virtual node_id new_node() const
     {
         static node_id counter = 0;
         return counter++;
     }
 
-    std::shared_ptr<Node> load_node(const node_id & id) const
+    virtual std::shared_ptr<Node> load_node(const node_id & id) const
     {
         return std::shared_ptr<Node>(copy_(storage_.at(id).get()));
     }
 
-    void delete_node(const node_id & id)
+    virtual void delete_node(const node_id & id)
     {
         storage_.erase(id);
     }
 
-    void write_node(const node_id & id, Node * node)
+    virtual void write_node(const node_id & id, Node * node)
     {
         storage_[id].reset(copy_(node));
     }
