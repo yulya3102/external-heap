cb564043fe8635d12f6fdb658e7baa1ef442cd0d
yulya3102 <yulya3102@gmail.com>
2016-04-10 22:42:41 +0300

    Improve test

--- heap/heap_tests.cpp
+++ heap/heap_tests.cpp
@@ -18,19 +18,22 @@ TEST(small, random)
     std::default_random_engine generator;
     std::uniform_int_distribution<std::int64_t> distribution(1, 100);
     data::heap<std::uint64_t, std::uint64_t> heap(5);
-    size_t elements = 512;
+    std::vector<std::pair<std::uint64_t, std::uint64_t>> elements;
+    size_t size = 512;
     heap.add(100, 100);
-    for (std::size_t i = 1; i < elements; ++i)
+    elements.push_back({100, 100});
+    for (std::size_t i = 1; i < size; ++i)
     {
         auto x = distribution(generator);
         heap.add(x, x);
+        elements.push_back({x, x});
     }
 
     std::vector<std::pair<std::uint64_t, std::uint64_t>> sorted;
     while (!heap.empty())
         sorted.push_back(heap.remove_min());
-    EXPECT_EQ(sorted.size(), elements);
-    EXPECT_TRUE(std::is_sorted(sorted.begin(), sorted.end()));
+    std::sort(elements.begin(), elements.end());
+    EXPECT_EQ(elements, sorted);
 }
 
 int main(int argc, char ** argv)
