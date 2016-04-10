1b220912208188cd5b36261ded2f911acbacbb05
yulya3102 <yulya3102@gmail.com>
2016-04-10 14:33:35 +0300

    Implement write_node

--- storage/directory.h
+++ storage/directory.h
@@ -6,6 +6,8 @@
 
 #include <boost/filesystem.hpp>
 #include <string>
+#include <fstream>
+#include <ios>
 
 namespace fs = boost::filesystem;
 
@@ -41,12 +43,19 @@ struct directory : basic_storage<Serialized>
 
     virtual void delete_node(const node_id & id)
     {
-        fs::remove(storage_dir / std::to_string(id));
+        fs::remove(node_path(id));
     }
 
     virtual void write_node(const node_id & id, Serialized * node)
     {
-        undefined;
+        std::ofstream out(node_path(id).string(), std::ios_base::binary);
+        out << node;
+        out.close();
+    }
+
+    fs::path node_path(const node_id & id)
+    {
+        return storage_dir / std::to_string(id);
     }
 
 private:
