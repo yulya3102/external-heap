5880a8f71f5f635f1eb591a234220ffd91e1146f
yulya3102 <yulya3102@gmail.com>
2016-04-10 23:11:37 +0300

    Add simple heap and compartion stubs

---
+++ simple/comparsion.cpp
@@ -0,0 +1,8 @@
+#include <utils/undefined.h>
+
+#include <gtest/gtest.h>
+
+int main()
+{
+    undefined;
+}
---
+++ simple/heap.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <utils/undefined.h>
+
+#include <utility>
+
+namespace simple
+{
+template <typename Key, typename Value>
+struct heap
+{
+    void add(Key k, Value v)
+    {
+        undefined;
+    }
+
+    std::pair<Key, Value> remove_min()
+    {
+        undefined;
+    }
+
+    bool empty()
+    {
+        undefined;
+    }
+};
+}
