e125989676285ac60a3336944e71b1d97891ed9a
yulya3102 <yulya3102@gmail.com>
2016-04-10 05:52:45 +0300

    Some more implementations

--- external_heap.h
+++ external_heap.h
@@ -29,29 +29,48 @@ namespace data
 template <typename Key, typename Value>
 struct heap
 {
-    heap(std::size_t t)
-        : big_elements(storage, t)
+    heap(Key small_max, std::size_t t)
+        : small_size(t)
+        , small_max(small_max)
+        , big(storage, t)
     {}
 
     void add(Key k, Value v)
     {
-        undefined;
+        if (k < small_max)
+            small_add(k, v);
+
+        big_add(k, v);
     }
 
     std::pair<Key, Value> remove_min()
     {
-        undefined;
+        if (small.empty())
+        {
+            auto out = std::back_inserter(small);
+            big.remove_left_leaf(out);
+        }
+
+        auto result = small.front();
+        small.pop_front();
+        return result;
     }
 
 private:
-    void small_add(std::int64_t x);
-    std::int64_t small_remove();
-    bool is_small(std::int64_t x);
-    void update_small_max();
+    void small_add(Key k, Value v)
+    {
+        undefined;
+    }
 
-    std::list<std::pair<Key, Value>> small_elements;
-    boost::optional<Key> small_max;
+    void big_add(Key k, Value v)
+    {
+        undefined;
+    }
+
+    std::size_t small_size;
+    Key small_max;
+    std::list<std::pair<Key, Value>> small;
     storage::memory<detail::b_node_data<Key, Value>> storage;
-    bptree::b_tree<Key, Value> big_elements;
+    bptree::b_tree<Key, Value> big;
 };
 }
