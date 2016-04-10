67c95feaad35daf5e0798360f519db30573783a6
yulya3102 <yulya3102@gmail.com>
2016-04-11 00:10:45 +0300

    Implement adding to heap

--- simple/heap.h
+++ simple/heap.h
@@ -1,17 +1,37 @@
 #pragma once
 
 #include <utils/undefined.h>
+#include <storage/directory.h>
 
 #include <utility>
+#include <algorithm>
 
 namespace simple
 {
 template <typename Key, typename Value>
 struct heap
 {
+    heap()
+        : nodes("simple_storage")
+    {}
+
     void add(Key k, Value v)
     {
-        undefined;
+        auto s = std::to_string(k) + " " + std::to_string(v);
+        auto id = next_id();
+        nodes.write_node(id, &s);
+        nodes_order.push_back(id);
+        std::make_heap(nodes_order.begin(), nodes_order.end(),
+                       [this] (Key a, Key b)
+        {
+            auto a_node = nodes.load_node(a);
+            auto b_node = nodes.load_node(b);
+
+            Key a_key = std::stoull(*a_node);
+            Key b_key = std::stoull(*b_node);
+
+            return a_key < b_key;
+        });
     }
 
     std::pair<Key, Value> remove_min()
@@ -23,5 +43,15 @@ struct heap
     {
         undefined;
     }
+
+private:
+    storage::node_id next_id() const
+    {
+        static storage::node_id id = 0;
+        return id++;
+    }
+
+    storage::directory<std::string> nodes;
+    std::vector<Key> nodes_order;
 };
 }
